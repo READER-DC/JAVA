260a4fd4cfdc4291e0b3204ee6e6ab3441c1fe37
Anatolii Sehal <sab198005@gmail.com>
2022-02-13 00:04:46 +0200

    Test OK!

--- src/Car/MainCar.java
+++ src/Car/MainCar.java
@@ -5,8 +5,16 @@ public class MainCar {
         Car car = new Car();
         Car audi = new Car();
         audi.refill(9);
+        System.out.println( audi.toString());
         audi.drive(10, 10);
+        System.out.println( audi.toString());
+        audi.refill(20);
+        System.out.println( audi.toString());
+        audi.drive(20, 20);
 
+        System.out.println( audi.toString());
+
+        audi.refill(50);
         System.out.println( audi.toString());
 
     }
